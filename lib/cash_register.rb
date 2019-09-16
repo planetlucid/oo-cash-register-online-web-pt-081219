7456e5b56a41e52e24129e3db69bf37d8016c92b
Ryan S <planetlucid@gmail.com>
2019-09-16 01:33:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,43 +1,34 @@
-
 class CashRegister
-
-  attr_accessor :total, :discount, :last_transaction_amount, :items
+  attr_accessor :total, :discount, :items
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
+    @last_transaction = []
   end
 
-
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
+  def add_item(item_name, price, quantity = 1)
+    @total += price * quantity
+    quantity.times { @items.push(item_name) }
+    @last_transaction.push([item_name, price, quantity])
   end
 
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
-      "There is no discount to apply."
-    end
-  end
+  def apply_discount
+    if discount == 0
+       "There is no discount to apply."
+     else
+       @total -= (@total * (@discount / 100.0)).to_i
+       "After the discount, the total comes to $#{@total}."
+     end
+   end
 
+   def items
+     @items
+   end
 
-  def void_last_transaction()
-    @total -= @last_transaction_amount
-  end
-
-end
+   def void_last_transaction
+     last_transaction = @last_transaction.pop
+     @total -= last_transaction[1] * last_transaction[2]
+   end
+end #CashRegister
